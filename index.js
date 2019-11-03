ab8e91cf9d806510dce9799973e773cd5db27feb
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-11-03 11:31:08 +0530

    add safe defaults

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const values = require('lodash.values');
 const semverCompare = require('semver-compare');
 const semverUtils = require('semver-utils');
 
-module.exports = function findLatestPatchVersions(versions) {
+module.exports = function findLatestPatchVersions(versions = []) {
   let groupedVersions = groupBy(
     versions.sort(semverCompare).map(semverUtils.parse),
     (v) => `${v.major}.${v.minor}`
