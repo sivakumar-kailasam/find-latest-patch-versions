d92fbf617346b782fd3d21b7eb415c83c88a450f
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-11-03 11:21:50 +0530

    Add code extracted from existing repo

---
+++ index.js
@@ -0,0 +1,13 @@
+const groupBy = require('lodash.groupby');
+const values = require('lodash.values');
+const semverCompare = require('semver-compare');
+const semverUtils = require('semver-utils');
+
+module.exports = function findLatestPatchVersions(versions) {
+  let groupedVersions = groupBy(
+    versions.sort(semverCompare).map(semverUtils.parse),
+    (v) => `${v.major}.${v.minor}`
+  );
+
+  return values(groupedVersions).map((verGroup) => verGroup.pop().version);
+};
